d516273ad2efa31e8008b18c0a0fc99b7c5a0df8
saiprakash32 <prakash.131@outlook.com>
2025-04-27 10:43:02 +0530

    new commit

---
+++ src/main/java/com/demo1/ArushApplication.java
@@ -0,0 +1,13 @@
+package com.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArushApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo1/controller/HellowWorldController.java
@@ -0,0 +1,15 @@
+package com.demo1.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HellowWorldController {
+
+			
+			@GetMapping("/arush")
+			public String getMessage(){
+				return "hi hello arush, Gud morning";
+				
+			}	
+		
+
+}
